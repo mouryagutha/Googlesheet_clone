132b4c2a0106ef88a16ba09430a5a5aae8178088
java_code <kmouryap@gmail.com>
2025-03-02 20:46:41 +0530

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import SpreadsheetPage from "./pages/Spreadsheet";
-import Home from "./pages/home";
+import Home from "./pages/Home";
 
 
 const App = () => {
